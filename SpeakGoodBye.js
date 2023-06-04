aeea47102279d4d537ccd325986570b89402e544
losikilove <75561119+losikilove@users.noreply.github.com>
2023-06-04 17:02:56 +0700

    harder

--- SpeakGoodBye.js
+++ SpeakGoodBye.js
@@ -21,7 +21,5 @@
 
     // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
     // 'byeSpeaker' on the global scope as well.
-    // xxxx.xxxx = byeSpeaker;
-
     window.byeSpeaker = byeSpeaker;
 })(window);
